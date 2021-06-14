5b5572aa3947d5fc5f915f5b10613f0a4f3740ed
Weebkun <2002umpa@gmail.com>
2021-06-14 17:24:06 +0800

    test time #1

--- db.js
+++ db.js
@@ -3,9 +3,13 @@ const {Sequelize} = require("sequelize");
 module.exports = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
     //host: "eu-cdbr-west-01.cleardb.com",
     dialect: "mysql",
+    timezone: "+08:00",
     define: {
         timestamps: false,
         freezeTableName: true
+    },
+    dialectOptions: {
+        useUTC: false
     },
     pool: {
         max: 5,
